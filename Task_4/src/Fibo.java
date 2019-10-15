1ff3ce72b697fe5b2fe6e75d1173fcb7aafcbda2
maevskij <56549797+maevskij@users.noreply.github.com>
2019-10-16 00:49:20 +0300

    Task_4

---
+++ Task_4/src/Fibo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Fibo {
+    public static void main (String[] args) {
+        // считывание переменной n
+        System.out.println("Введите n:");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int x0 = 1;
+        int x1 = 1;
+        int x2;
+        System.out.print(x0+" "+x1+" ");
+        for(int i = 3; i <= n; i++){
+            x2=x0+x1;
+            System.out.print(x2+" ");
+            x0=x1;
+            x1=x2;
+        }
+        System.out.println();
+    }
+}
